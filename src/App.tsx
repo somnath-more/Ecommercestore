059ba49c41b23aef2157be787a4fcf0bb4686867
Somnath  More <somnath.more@zemosolabs.com>
2023-10-23 21:17:52 +0530

    improved Scaffold

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
 
     <>
-    <h1>Hellow World</h1>
+  <h1>Hello World</h1>
     </>
   );
 }
